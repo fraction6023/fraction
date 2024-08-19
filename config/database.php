51d8f6aaaaeee8a2864e100276449bca01e39d31
my name <my email>
2024-08-19 12:08:48 +0300

    تعريب صفحة الدخول

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '8889'),
             'database' => env('DB_DATABASE', 'fraction'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),

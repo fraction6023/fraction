1214828aecdd5c99eda8982caa72a62ce36e4977
my name <my email>
2024-10-12 10:44:27 +0300

    fixing apple devices qr reader page

--- resources/views/gym/qrScanner.blade.php
+++ resources/views/gym/qrScanner.blade.php
@@ -4,13 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
     <div class="container col-lg-6 py-4">
         <div class="card bg-white shadow rounded-3 p-3 border-0">
-            <p style="text-align: center;">امسح الباركود للدخول</p>
-            <video id="preview" class="video-background" autoplay loop muted playsinline></video>
+            <p style="text-align: center;">امسح الباركود للدخول</p>            
+            <video id="preview"></video>
+            <!-- <video id="preview" class="video-background" autoplay loop muted playsinline></video> -->
         </div>
     </div>
 
@@ -30,6 +31,8 @@
         console.error(e);
       });
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
   </body>
 </html>

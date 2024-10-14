662a80cc4470de15cd14889d2d669b1667382e2c
my name <my email>
2024-10-14 22:25:10 +0300

    ios qr scanner issue

--- resources/views/gym/qrScanner.blade.php
+++ resources/views/gym/qrScanner.blade.php
@@ -49,11 +49,15 @@
       Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length = 1) {
           scanner.start(cameras[0]);
-        }else {
+        }else if(cameras.length = 2) {
             scanner.start(cameras[1]);
-          //console.error('No cameras found.');
+        }else if(cameras.length = 3) {
+            scanner.start(cameras[2]);
+        }else if(cameras.length = 4) {
+            scanner.start(cameras[3]);
+        }else {
+          console.error('No cameras found.');
         }
-         
       }).catch(function (e) {
         console.error(e);
       });

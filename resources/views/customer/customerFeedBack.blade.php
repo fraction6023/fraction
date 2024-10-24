5a8168c7eaa2b6b623b2c47af4d3cfb5545716cc
my name <my email>
2024-10-24 22:33:32 +0300

    update customer feedback display

--- resources/views/customer/customerFeedBack.blade.php
+++ resources/views/customer/customerFeedBack.blade.php
@@ -7,11 +7,11 @@
     <div class="card center">
         <p class="">{{$feedback->gym->name}}</p>
         <div class="card-header ">
-        @if( $feedback->gym_rate -1 >=0 ) <span class="fa fa-star checked"></span> @else <span class="fa fa-star"></span> @endif
-        @if( $feedback->gym_rate -2 >=0 ) <span class="fa fa-star checked"></span> @else <span class="fa fa-star"></span> @endif
-        @if( $feedback->gym_rate -3 >=0 ) <span class="fa fa-star checked"></span> @else <span class="fa fa-star"></span> @endif
-        @if( $feedback->gym_rate -4 >=0 ) <span class="fa fa-star checked"></span> @else <span class="fa fa-star"></span> @endif
-        @if( $feedback->gym_rate -5 >=0 ) <span class="fa fa-star checked"></span> @else <span class="fa fa-star"></span> @endif
+        @if( $feedback->customer_rate -1 >=0 ) <span class="fa fa-star checked"></span> @else <span class="fa fa-star"></span> @endif
+        @if( $feedback->customer_rate -2 >=0 ) <span class="fa fa-star checked"></span> @else <span class="fa fa-star"></span> @endif
+        @if( $feedback->customer_rate -3 >=0 ) <span class="fa fa-star checked"></span> @else <span class="fa fa-star"></span> @endif
+        @if( $feedback->customer_rate -4 >=0 ) <span class="fa fa-star checked"></span> @else <span class="fa fa-star"></span> @endif
+        @if( $feedback->customer_rate -5 >=0 ) <span class="fa fa-star checked"></span> @else <span class="fa fa-star"></span> @endif
         </div>
         
         <div class="card-body">

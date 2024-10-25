c5fc8b6e5683a00f47b1783fa0377fa012c470ee
my name <my email>
2024-10-25 16:10:10 +0300

    updating gyms map show

--- resources/views/customer/showGymsOnMap.blade.php
+++ resources/views/customer/showGymsOnMap.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="card">
-    <div class="card-header"><p class="">استعراض الأندية على الخريطة</p></div>
+    <div class="card-header center"><p class="">استعراض الأندية على الخريطة</p></div>
     <div class="card-body">
          <div class="container" style="margin-top: 50px;">
         <div id="map"></div>
@@ -20,15 +20,20 @@
     function initMap() {
         const latLng = { lat: 26.2555032, lng: 44.0218632 };
         const mapOptions = {
-            zoom: 6,
+            zoom: 10,
             center: latLng,
         };
         const map = new google.maps.Map(document.getElementById("map"), mapOptions);
         new google.maps.Marker({
-            position: latLng,
+            position: { lat: 26.2555032, lng: 44.0218632 },
             map: map,
             title: "نادي كل الرياضة",
         });
+        new google.maps.Marker({
+            position: { lat: 26.2955032, lng: 44.0369632 },
+            map: map,
+            title: "body motion",
+        })
     }
     window.initMap = initMap;
 </script>

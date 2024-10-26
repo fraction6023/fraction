934ef5ad4b4d64d03f109815f841de9f3fd45d8b
my name <my email>
2024-10-27 01:50:07 +0300

    add collected and not collected amount

--- resources/views/gym/finance.blade.php
+++ resources/views/gym/finance.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 
 @php
-$amount = 0;
+$amountNotCollected = 0;
+$amountCollected = 0;
 @endphp
 <div class="card center" style="margin-bottom: 5px;">
     <div class="card-body">
@@ -26,15 +27,24 @@ $amount = 0;
                 <!-- <td class=""><div class=""><p class="">وقت الزيارة</p></div></td> -->
                 <td class=""><div class=""><p class="">{{$visit->updated_at}}</p></div></td>
                 <!-- <td class=""><div class=""><p class="">الحالة</p></div></td> -->
-                <td class=""><div class=""><p class="">لم يتم التحصيل</p></div></td>
+                @if($visit->status =! 'collected')
+                    <td class=""><div class=""><p class="">تم التحصيل</p></div></td>
+                    $amountCollected = $amountCollected + $visit->cost;
+                @else
+                    <td class=""><div class=""><p class="">لم يتم التحصيل</p></div></td>
+                @php 
+                    $amountCollected = $amountCollected + $visit->cost;
+                @endphp
+                
+                @endif
             </tr>
-            @php $amount = $amount + $visit->cost @endphp
+            <!-- @php $amount = $amountCollected + $visit->cost @endphp -->
     @endforeach
         </table>
     </div>
 </div>
 
-<div class="card center">
+<!-- <div class="card center">
     <table style="width: 100%;">
         <tr>
             <td style="width:50%;text-align: left; padding-left:15px;font-weight:bold">
@@ -44,10 +54,36 @@ $amount = 0;
             </td>
             <td style="width: 50%; text-align:right;padding-right:15px">
                 <div>
-                    <p class="">{{$amount}}</p>
+                    <p class="">{{$amountCollected}}</p>
                 </div>
             </td>
         </tr>
     </table>
+</div> -->
+
+<div class="card center">
+    <table style="width: 100%;">
+        <tr>
+            <td style="width:50%;text-align: left; padding-left:15px;font-weight:bold">
+                <p class="">تم تحصيله </p>
+            </td>
+            <td style="width: 50%; text-align:right;padding-right:15px">
+                <p class="">{{$amountCollected}}</p>
+            </td>
+        </tr>
+    </table>
 </div>
+<div class="card center">
+    <table style="width: 100%;">
+        <tr>
+            <td style="width:50%;text-align: left; padding-left:15px;font-weight:bold">
+                <p class="">لم يتم تحصيله </p>
+            </td>
+            <td style="width: 50%; text-align:right;padding-right:15px">
+                <p class="">{{$amountNotCollected}}</p>
+            </td>
+        </tr>
+    </table>
+</div>
+
 @endsection
